47223cb8198af61f4d5a179b5148ec233cea1d00
lcbjrrr <luiz.junior1@ibmec.edu.br>
2020-09-07 20:18:04 -0300

    sem certeza de nada

--- Code_r/metricas_fusão_flev_f1.r
+++ Code_r/metricas_fusão_flev_f1.r
@@ -34,11 +34,11 @@ mat <- matrix(mat,  ncol = 10, byrow = TRUE)
 nk <- 10
 x       <- seq(0, nk - 1, 1)
 freq_f1 <- matrix(0, 0, nk)
-freq_f2 <- matrix(0, 0, nk)
-freq_f3 <- matrix(0, 0, nk)
-freq_f4 <- matrix(0, 0, nk)
-freq_f5 <- matrix(0, 0, nk)
-freq_f6 <- matrix(0, 0, nk)
+#freq_f2 <- matrix(0, 0, nk)
+#freq_f3 <- matrix(0, 0, nk)
+#freq_f4 <- matrix(0, 0, nk)
+#freq_f5 <- matrix(0, 0, nk)
+#freq_f6 <- matrix(0, 0, nk)
 for(k in 1: nk){
 	freq_f1[k] <- mat[1, k]
 #	freq_f2[k] <- mat[2, k]
@@ -47,18 +47,15 @@ for(k in 1: nk){
 #	freq_f5[k] <- mat[5, k]
 #	freq_f6[k] <- mat[6, k]
 }
-df <- data.frame(x = x, y1 = freq_f1, y2 = freq_f2, y3 = freq_f3, y4 = freq_f4, y5 = freq_f5, y6 = freq_f6)
-alpha <- c(1,2,3,4,5,6)
+#df <- data.frame(x = x, y1 = freq_f1, y2 = freq_f2, y3 = freq_f3, y4 = freq_f4, y5 = freq_f5, y6 = freq_f6)
+df <- data.frame(x = x, y1 = freq_f1)
+
+alpha <- c(1)
 p <- ggplot(df) 
-pp <- p + geom_line(aes(x = x, y = y1, color = "Aver")  , size=4, alpha=.7) +
-          geom_line(aes(x = x, y = y2, color = "PCA")   , size=4, alpha=.7, linetype = 3) +
-	        geom_line(aes(x = x, y = y4, color = "MR-DWT"), size=4, alpha=.7) +
-          geom_line(aes(x = x, y = y3, color = "MR-SWT"), size=4, alpha=.7) +
-          geom_line(aes(x = x, y = y5, color = "ROC")   , size=4, alpha=.7) +
-          geom_line(aes(x = x, y = y6, color = "MR-SVD"), size=4, alpha=.7) +
-  ylim(.01,1) +
-	 ylab(TeX('Probability')) +
-	 xlab(TeX('Detection error')) +
+pp <- p + geom_line(aes(x = x, y = y1, color = "Aver")  , size=4, alpha=.7) 
+    ylim(.01,1) +
+	ylab(TeX('Probability')) +
+	xlab(TeX('Detection error')) +
   theme_ipsum(base_family = "Times New Roman", 
               base_size = 20, axis_title_size = 20) +
   scale_fill_ipsum() +
